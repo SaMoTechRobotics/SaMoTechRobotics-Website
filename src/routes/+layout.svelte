77c88a7fca2309059cc7dcf46317d79e72005aaa
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-07-25 20:43:29 -0700

    Added layout and global styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<slot></slot>
+
+<style lang="scss">
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background-color: var(--bg);
+		color: var(--text);
+    font-family: var(--font);
+  }
+
+  :root {
+    --bg: rgb(239, 253, 255);
+    --bg-hover: rgb(203, 222, 228);
+    --bg-secondary: #f5f5f5;
+
+    --primary: rgba(0, 200, 255, 1);
+    --secondary: rgb(77, 216, 254);
+    --dark: rgba(2, 145, 184, 1);
+
+    --text: rgb(10, 100, 124);
+    --text-gray: rgb(54, 89, 98);
+    --text-muted: rgb(0, 139, 177);
+    --text-inv: #fff;
+
+    --text-shadow: 0 2px 2px rgba(0, 0, 0, 0.3);
+    --text-shadow-hover: 0 3px 3px rgba(0, 0, 0, 0.3);
+    --box-shadow: 0 3px 3px rgba(0, 0, 0, 0.3);
+    --box-shadow-hover: 0 3px 3px rgba(0, 0, 0, 0.5);
+
+    --img-shadow: 0 3px 3px rgba(0, 0, 0, 0.3);
+
+    --font: "Roboto", sans-serif;
+  }
+</style>
