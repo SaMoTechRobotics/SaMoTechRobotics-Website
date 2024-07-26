a9a93faaee5165bd52e47569515ae28232c064d6
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-07-26 15:40:20 -0700

    Started home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,33 @@
-<h1>New SaMoTechRobotics Website</h1>
+<div>
+	<h1>SaMoTech Robotics</h1>
+
+	<p>
+		SaMoTech is a 501c3 community-based robotics team competing in the FIRST Tech Challenge (FTC). Based in Santa
+		Monica, CA, our mission is to inspire young people to be the science and technology leaders of the future. Through
+		participation in FTC, we develop hands-on skills in engineering, robotics, and coding while improving life-skills
+		like teamwork and self-confidence.
+
+		SaMoTech currently has 8 members, aged 13-15, all hard-working and high-achieving students from diverse backgrounds
+		and with varied interests, each bringing a vital perspective necessary for our success. We students are responsible
+		for the design and construction of our robots, using a variety of tools and techniques, with guidance from our
+		volunteer coaches and mentors. We also collaborate on marketing and outreach efforts, helping to promote the team
+		and engage with our community. In particular, we dedicate many hours to helping other teams and schools.
+	</p>
+</div>
+
+
+<style lang="scss">
+  div {
+    background-color: var(--primary);
+    height: 100%;
+
+    h1 {
+      font-size: 3rem;
+    }
+
+    p {
+      font-size: 1.2rem;
+      line-height: 2rem;
+    }
+  }
+</style>
