de15deb901d04fb17441c95720e3f04b9bd5682b
bebuckle <bebuckle@gmail.com>
2025-01-17 14:48:23 -0800

    Tweaked to have two teams and 13 students

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,15 +8,15 @@
 
 <Card img="images/2024-25/SaMoTech-team_on-grass.jpg" title="About Us" textSide="right" dark>
 	<p>
-		SaMoTech is a 501c3 community-based robotics team competing in the FIRST Tech Challenge (FTC). Based in Santa
+		SaMoTech is a 501c3 community-based robotics program competing in the FIRST Tech Challenge (FTC). Based in Santa
 		Monica, CA, our mission is to inspire young people to be the science and technology leaders of the future. Through
 		participation in FTC, we develop hands-on skills in engineering, robotics, and coding while improving life-skills
 		like teamwork and self-confidence.
 
-		SaMoTech currently has 9 members, aged 14-17, all hard-working and high-achieving students from diverse backgrounds
+		SaMoTech currently has two teams with a total of 13 members, aged 12-17, all hard-working and high-achieving students from diverse backgrounds
 		and with varied interests, each bringing a vital perspective necessary for our success. We students are responsible
 		for the design and construction of our robots, using a variety of tools and techniques, with guidance from our
-		volunteer coaches and mentors. We also collaborate on marketing and outreach efforts, helping to promote the team
+		coaches and mentors. We also collaborate on marketing and outreach efforts, helping to promote the team
 		and engage with our community. In particular, we dedicate many hours to helping other teams and schools.
 	</p>
 </Card>
