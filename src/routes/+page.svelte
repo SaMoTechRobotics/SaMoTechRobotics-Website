ab063105f063278a87902a4bdd30f7e7d06e9f7f
BrandonKirbyson <brandonkirbyson@gmail.com>
2026-03-28 09:52:40 -0700

    About Updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,21 +36,21 @@
 <SponsorsSection small />
 
 <SplitSection img="images/2024-25/230909_Kickoff-Field.jpg" title="About Us" textSide="right" dark>
-	SaMoTech NOW is a 501c3 community-based robotics team competing in the FIRST Tech Challenge (FTC). Based in Santa
-	Monica,
-	CA, our mission is to inspire young people to be the science and technology leaders of the future. Through
+	SaMoTech is a 501c3 community-based robotics program competing in the FIRST Tech Challenge (FTC). Based in Santa
+	Monica, CA, our mission is to inspire young people to be the science and technology leaders of the future. Through
 	participation in FTC, we develop hands-on skills in engineering, robotics, and coding while improving life-skills like
 	teamwork and self-confidence.
 
 	<br />
 
-	SaMoTech currently has 9 members, aged 14-16, all hard-working and high-achieving students from diverse backgrounds
-	and with varied interests, each bringing a vital perspective necessary for our success.
+	SaMoTech currently has two teams with 15 members total: 10 members on SaMoTech and 5 members on Cyberquakes, ranging
+	in age from 13 to 18. Our hard-working and high-achieving students come from diverse backgrounds and bring varied
+	interests, with each member contributing a vital perspective to the team.
 	<br />
 	We students are responsible for the design and construction of our robots, using a variety of tools and techniques,
-	with guidance from our
-	volunteer coaches and mentors. We also collaborate on marketing and outreach efforts, helping to promote the team and
-	engage with our community. In particular, we dedicate many hours to helping other teams and schools.
+	with guidance from our volunteer coaches and mentors. We also collaborate on marketing and outreach efforts, helping
+	to promote the team and engage with our community. In particular, we dedicate many hours to helping other teams and
+	schools.
 
 	<div slot="buttons">
 		<LinkButton to="/support" primary>Support Us</LinkButton>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,19 +4,19 @@
 	import Card from '$lib/components/sections/SplitSection.svelte';
 </script>
 
-<Card img="images/2024-25/SaMoTech-team_on-grass.jpg" title="About Us" textSide="right" dark>
+<Card img="images/Hero/2026Regionals.jpg" title="About Us" textSide="right" dark>
 	<p>
 		SaMoTech is a 501c3 community-based robotics program competing in the FIRST Tech Challenge
 		(FTC). Based in Santa Monica, CA, our mission is to inspire young people to be the science and
 		technology leaders of the future. Through participation in FTC, we develop hands-on skills in
 		engineering, robotics, and coding while improving life-skills like teamwork and self-confidence.
-		SaMoTech currently has two teams with a total of 13 members, aged 12-17, all hard-working and
-		high-achieving students from diverse backgrounds and with varied interests, each bringing a
-		vital perspective necessary for our success. We students are responsible for the design and
-		construction of our robots, using a variety of tools and techniques, with guidance from our
-		coaches and mentors. We also collaborate on marketing and outreach efforts, helping to promote
-		the team and engage with our community. In particular, we dedicate many hours to helping other
-		teams and schools.
+		SaMoTech currently has two teams with 15 members total: 10 members on SaMoTech and 5 members on
+		Cyberquakes, ranging in age from 13 to 18. Our hard-working and high-achieving students come
+		from diverse backgrounds and bring varied interests, with each member contributing a vital
+		perspective to the team. We students are responsible for the design and construction of our
+		robots, using a variety of tools and techniques, with guidance from our coaches and mentors. We
+		also collaborate on marketing and outreach efforts, helping to promote the team and engage with
+		our community. In particular, we dedicate many hours to helping other teams and schools.
 	</p>
 </Card>
 
@@ -91,7 +91,13 @@
 
 <ImageGallery
 	images={[
+		'images/Hero/2026Regionals.jpg',
+		'images/Hero/Franklin 2.jpg',
+		'images/Hero/ILT6.jpg',
+		'images/Hero/Regionals_06_FinalQ.jpg',
 		'images/2023-24/ILT2_1st_Samo-Grp.jpg',
+		'images/News/Promenade.jpg',
+		'images/News/InnovationHub.jpg',
 		'images/2024-25/231202_Samo_Meet1_Practice.jpg',
 		'images/2024-25/Samo-CDs-Trophies_240316.jpg',
 		'images/2023-24/CAD-Seminar.jpg',
